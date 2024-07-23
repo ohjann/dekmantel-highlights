197869621da9397147bc0d6de7d4c62ef8112d2f
Eoghan Hynes <eoghan@hyn.es>
2024-07-23 16:52:40 +0100

    Add grouping

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,39 @@
 <script>
+  import dayjs from 'dayjs';
   export let data;
   const { artists } = data;
+  const groupActsByStartDate = (timetable) => {
+    return timetable.reduce((groupedActs, act) => {
+      const startDate = dayjs(act.startDate).format('dddd, D MMM');
+      if (!groupedActs[startDate]) {
+        groupedActs[startDate] = [];
+      }
+      groupedActs[startDate].push(act);
+      return groupedActs;
+    }, {});
+  }
+  const groupedActs = groupActsByStartDate(artists)
+  Object.keys(groupedActs).forEach(date => {
+    groupedActs[date].sort((a, b) => dayjs(a.startDate).diff(dayjs(b.startDate)));
+  });
 </script>
 
-{#each artists as artist}
-	<h1>{artist.artistName}</h1>
-  <span>{artist.rating}</span>
-  <p>{artist.description}</p>
-    <a href={artist.youtubeLink}>{artist.youtubeLink}</a>
-    <a href={artist.dekmentalLink}>{artist.dekmentalLink}</a>
+{#each Object.entries(groupedActs) as [date, acts]}
+  <div class="relative bg-green-50 m-10 p-10">
+    <h1>{date}</h1>
+    {#each acts as act}
+      <div class="bg-gray-100 w-1/3 m-2 rounded-md p-2">
+      <h1 class="text-lg font-bold">{act.artistName}</h1>
+      <div class="bg-green-200 w-[300px] flex">
+        <div class="bg-red-400" style="width: {act.rating * 10}%">{act.rating}</div>
+      </div>
+      <p>{act.description}</p>
+      <a href={act.youtubeLink}>{act.youtubeLink}</a>
+      <a href={act.dekmentalLink}>{act.dekmentalLink}</a>
+      <p>{act.startDate}</p>
+      <p>{act.endDate}</p>
+      <img src={act.image} />
+      </div>
+    {/each}
+  </div>
 {/each}
