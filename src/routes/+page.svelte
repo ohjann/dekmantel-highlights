a51ef0d53986d806a098ad9f72d15d7e85d0b9a9
Eoghan Hynes <eoghan@hyn.es>
2024-07-23 16:52:40 +0100

    Dekmentel-ish styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,36 +18,36 @@
 </script>
 
 {#each Object.entries(artists) as [date, acts]}
-  <div class="relative bg-green-50">
-    <h1 class="text-xl font-bold">{date}</h1>
+  <div class="relative bg-[#8fa1ae]">
+    <h1 class="text-xl font-custom text-[#92ff97]">{date}</h1>
     <Timeline position="alternate">
       {#each acts as act}
         <TimelineItem>
           {#if act.startDate}
           <TimelineOppositeContent slot="opposite-content">
-            <p>{dayjs(act.startDate).format("HH:mm")}–{dayjs(act.endDate).format("HH:mm")}</p>
+            <p class="font-custom">{dayjs(act.startDate).format("HH:mm")}–{dayjs(act.endDate).format("HH:mm")}</p>
           </TimelineOppositeContent>
           {/if}
           <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
+            <TimelineDot style="background: #af5339; border-color: #af5339"/>
+            <TimelineConnector style="background: #af5339"/>
           </TimelineSeparator>
           <TimelineContent>
-        <Card.Root>
+            <Card.Root class="bg-[#8fa1ae] border-[#6a6b59] border-4 rounded-none">
           <Card.Header>
-            <div class="relative w-full h-[112.5px] rounded-md overflow-hidden">
+            <div class="relative w-full h-[112.5px] overflow-hidden">
               <a href={act.dekmentalLink} target="_blank">
-                <img src={act.image} class="w-full h-full object-cover rounded-md border-0">
+                <img src={act.image} class="w-full h-full object-cover border-0">
               </a>
             </div>
-            <Card.Title>{act.artistName}</Card.Title>
+            <Card.Title class="font-custom tracking-wide text-[#6a6b59]">{act.artistName}</Card.Title>
             <Card.Description>
               <Progress value={act.rating*10} />
             </Card.Description>
           </Card.Header>
           <Card.Content>
             <p>{act.description}</p>
-            <a class="text-red-500" href={act.youtubeLink} target="_blank">Music sample</a>
+            <a class="text-[#af5339]" href={act.youtubeLink} target="_blank">Music sample</a>
             {#if act.overlappingArtists.length}
               <p>Overlapping with: <span class="italic">{act.overlappingArtists.join(", ")}</span></p>
             {/if}
