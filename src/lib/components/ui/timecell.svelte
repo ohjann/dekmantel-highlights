f906dddc85554d5ed01624fff6e83e6789a76147
Eoghan Hynes <eoghan@hyn.es>
2024-07-26 12:00:52 +0100

    Nuke timetable elements

--- src/lib/components/ui/timecell.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  	export let time;
-</script>
-
-<div class="row-span-4 text-[#def1cb] block relative text-xs sm:text-base"><span class="absolute -bottom-2 sm:-bottom-3 w-[0.8rem] -right-[0.3rem] sm:-right-3 sm:w-5 bg-[#FFB076] [writing-mode:vertical-lr] sm:[writing-mode:initial]">{time}</span></div>
